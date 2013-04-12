c8d73cfd9d13d40918a323b7d2b3bb3cf007deb0
Kim Søyland-Lier <soykim11@nith.no>
2013-04-12 21:20:07 +0200

    Lagt til en HighScore klasse

---
+++ DragonGame1/DragonGame1/HighScore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DragonGame1
+{
+    [Serializable]
+    public class HighScore
+    {
+        //List that holds on to the best scores.
+        private List<Score> HighScoreList;
+
+        public HighScore() {
+            HighScoreList = new List<Score>();
+        }
+
+        public void SetHighScore(List<Score> highScore) {
+            HighScoreList = highScore;
+        }
+
+        public List<Score> GetHighScore() {
+            return HighScoreList;
+        }
+
+    }
+
+    [Serializable]
+    public class Score {
+        public string PlayerName { get; set; }
+        public int Score { get; set; }
+    }
+}
